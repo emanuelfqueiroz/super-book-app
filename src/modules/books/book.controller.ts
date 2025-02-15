7665a6a398066b113cf7efff9c2fd6f108d2545b
Emanuel Q <emanuel@eturingsolutions.com>
2025-02-15 16:33:22 -0300

    prototype version 1.0.1

--- src/modules/books/book.controller.ts
+++ src/modules/books/book.controller.ts
@@ -22,6 +22,11 @@ export class BookController {
   about() {
     return { title: 'About Page' };
   }
+  @Get('/pricing')
+  @Render('pricing')
+  pricing() {
+    return { title: 'Pricing Page' };
+  }
   @Get('/book/:id')
   @Render('book')
   async getBook(@Param('id') id: string) {
@@ -36,6 +41,8 @@ export class BookController {
 
   @Get('/book/_partials/recent')
   async getRecentBooks(@Res() res: Response) {
+    await new Promise(resolve => setTimeout(resolve, 3000));
+
     const recentBooks = await this.service.getRecentBooks(5); // get last 5 books
     return res.render('partials/recent-books', {
       layout: false,
@@ -45,6 +52,7 @@ export class BookController {
 
   @Get('/book/_partials/analysis/:id')
   async getAnalysis(@Res() res: Response, @Param('id') id: string) {
+    await new Promise(resolve => setTimeout(resolve, 3000));
     const analysis = await this.service.getAnalysis(id);
     return res.render('partials/books/analysis', {
       layout: false,
